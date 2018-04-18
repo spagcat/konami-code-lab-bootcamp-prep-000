31d4e95675919918246a4044f83517090a2272be
Shel Orock <sdo243@nyu.edu>
2018-04-18 19:49:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,17 +3,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  function keyListener(e) {
+  function keyListener('keydown', (e) {
     const key = parseInt(e.detail || e.which || e.location);
     
     if(key === code[index]) {
       index++;
-      if (index === code.length) {
+      if (index === code.length - 1) {
         alert('Congrats!');
         index = 0;
       }
     } else {
       index = 0;
     }
-  }
+  } )
 }
