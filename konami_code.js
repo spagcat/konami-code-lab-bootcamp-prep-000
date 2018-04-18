d47d3cc8a96bdb5e9750705f84faeca9a717fcd7
Shel Orock <sdo243@nyu.edu>
2018-04-18 19:49:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0;
-
 function init() {
+  let index = 0;
   function keyListener('keydown', (e) {
     const key = parseInt(e.detail || e.which || e.location);
     
