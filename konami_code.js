cfeeecb55a5d25c315714008407ea0bbf715419d
Shel Orock <sdo243@nyu.edu>
2018-04-18 19:30:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  const input = document.querySelector('input');
-  input.addEventListener('keydown', function(e) {
-    let index = 0;  
-    keydown = parseInt(e.detail || e.which || e.location);
-    if (keydown === code[index]) {
-      index++;
-      if (index === code.length) {
-        alert ('Congratulations!');
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  })
+  
 }
